80473629ae68dc663b3a538aa375ec8888c1343e
EL3VT <93453765+EL3VT@users.noreply.github.com>
2026-03-14 13:53:13 +0300

    Added solution to Even the Last problem

---
+++ Elementary/Even the Last.py
@@ -0,0 +1,20 @@
+def checkio(array):
+    number = 0
+    if len(array) > 0:
+        for i in range(0, len(array), 2):
+            number += array[i]
+        
+        number *= array[-1]
+    
+    return number
+
+print("Example:")
+print(checkio([0, 1, 2, 3, 4, 5]))
+
+# These "asserts" are used for self-checking
+assert checkio([0, 1, 2, 3, 4, 5]) == 30
+assert checkio([1, 3, 5]) == 30
+assert checkio([6]) == 36
+assert checkio([]) == 0
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")

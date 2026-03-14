e4b82ab05947b328a5bdecef0862d3e462e091b0
EL3VT <93453765+EL3VT@users.noreply.github.com>
2026-03-14 13:53:55 +0300

    Added solution to the Right to Left problem

---
+++ Elementary/Right to Left.py
@@ -0,0 +1,19 @@
+def left_join(phrase):
+    output_array = []
+    
+    for i in phrase:
+        output_array.append(i.replace("right", "left"))
+            
+    return ",".join(output_array)
+
+
+print("Example:")
+print(left_join(("left", "right", "left", "stop")))
+
+# These "asserts" are used for self-checking
+assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop"
+assert left_join(("bright aright", "ok")) == "bleft aleft,ok"
+assert left_join(("brightness wright",)) == "bleftness wleft"
+assert left_join(("enough", "jokes")) == "enough,jokes"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")

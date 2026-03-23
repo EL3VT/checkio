8c496c2d341a127ea0134e33e10393a065e09db1
EL3VT LAB <93453765+EL3VT@users.noreply.github.com>
2026-03-23 19:17:29 +0300

    Added solution to the Backward Each Word problem

---
+++ Elementary+/Backward Each Word.py
@@ -0,0 +1,16 @@
+def backward_string_by_word(text):
+    splited_text = text.split(" ")
+    return " ".join([text_element[::-1] for text_element in splited_text])
+
+
+print("Example:")
+print(backward_string_by_word(""))
+
+# These "asserts" are used for self-checking
+assert backward_string_by_word("") == ""
+assert backward_string_by_word("world") == "dlrow"
+assert backward_string_by_word("hello world") == "olleh dlrow"
+assert backward_string_by_word("hello   world") == "olleh   dlrow"
+assert backward_string_by_word("welcome to a game") == "emoclew ot a emag"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
